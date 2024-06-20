fd9cb0ba8fa607eae2a74c023e33bcd5ba54a533
biel.lil <biel1313biel@gmail.com>
2024-06-20 12:01:21 -0300

    feat: ✨ global styles

---
+++ web/src/styles.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+export const MainHero = styled.section`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  background-color: rgb(15 15 15 / 0.84);
+  height: 100vh;
+  width: 100vw;
+  .container {
+    width: 100%;
+    max-width: 70rem;
+
+    margin-top: 25rem;
+    margin-right: auto;
+    margin-left: auto;
+    padding: 1rem 0.5rem;
+    margin-bottom: 1rem;
+  }
+  @media (max-width: 768px) {
+    .container {
+      width: 90%;
+      max-width: 68rem;
+    }
+  }
+`
---
+++ web/src/styles/global.ts
@@ -0,0 +1,48 @@
+import { createGlobalStyle } from 'styled-components'
+export const GlobalStyle = createGlobalStyle`
+
+
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: none;
+    border: none;
+    text-decoration: none;
+    
+    
+  font-family: sans-serif;
+  
+  html {
+   
+    font-size: 100%;
+  }
+
+  body {
+  height: 100vh;
+  margin: 0;
+ width: 100vw;
+}
+
+  @media (max-width: 768px) {
+    html {
+      font-size: 80%;
+    }
+  }
+
+  @media (max-width: 600px) {
+    html {
+      font-size: 65%;
+    }
+  }
+  @media (max-width: 425px) {
+    html {
+      font-size: 59%;
+    }
+  }
+
+}
+
+
+`
