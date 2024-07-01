cb73e5e9a86112736aa1648cf686c0974091dd22
biel.lil <biel1313biel@gmail.com>
2024-07-01 14:56:46 -0300

    # Informações adicionais Fonte do commit: message SHA do commit:

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import * as ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
