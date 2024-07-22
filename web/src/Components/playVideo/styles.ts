bc5856fed72b1b1ac0d282139196dad9bda0c7ac
biel.lil <biel1313biel@gmail.com>
2024-07-22 16:55:17 -0300

    Update styles.ts

--- web/src/Components/playVideo/styles.ts
+++ web/src/Components/playVideo/styles.ts
@@ -4,20 +4,34 @@ export const Herosection = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 2rem;
+  gap: 1rem;
+
+  .css-151mcfm.Mui-expanded {
+    margin: 0;
+  }
 `
 export const Accordiondiv = styled.div`
-  width: 50vw;
+  width: 49vw;
+  padding: 1rem 3vw;
+  display: flex;
+  gap: 0.5rem;
+  flex-direction: column;
+
   .text {
     div {
       display: flex;
       align-items: center;
       justify-content: center;
+      margin: 10px 0 0 0;
     }
   }
   .Player {
+    justify-content: center;
     display: flex;
     align-items: center;
-    justify-content: center;
+  }
+
+  .MuiAccordion-region {
+    height: 35vh;
   }
 `
