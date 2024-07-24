4a354a5f083e9f3251694e5790bac21cb5c9a12d
biel.lil <biel1313biel@gmail.com>
2024-07-24 15:10:04 -0300

    ✨ Criar componente Categoy e seus estilos

---
+++ src/pages/category/index.tsx
@@ -0,0 +1,11 @@
+import { CategoryContainer } from './styles'
+
+export function Category() {
+  return (
+    <>
+      <CategoryContainer>
+        <h1>Category</h1>
+      </CategoryContainer>
+    </>
+  )
+}
---
+++ src/pages/category/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const CategoryContainer = styled.section`
+  max-width: 50vw;
+  width: 46vw;
+`
