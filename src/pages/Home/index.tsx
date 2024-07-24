8225605669d413cdb5a734e2c30ccac5d6ce33a9
biel.lil <biel1313biel@gmail.com>
2024-07-24 15:09:24 -0300

    ✨ Criar componente Home e seus estilos

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,85 @@
+import React, { useState, useEffect, useCallback } from 'react'
+import { Pagination, Alert } from '@mui/material'
+import {
+  HomeContainer,
+  MoviesGrid,
+  MovieCard,
+  MovieBanner,
+  MovieTitle,
+} from './styles'
+
+export function Home() {
+  const [movies, setMovies] = useState([])
+  const [page, setPage] = useState(1)
+  const [totalPages, setTotalPages] = useState(1)
+  const [error, setError] = useState(null)
+
+  const fetchMovies = useCallback(async (page: number) => {
+    const cacheKey = `movies_page_${page}`
+    const cachedData = localStorage.getItem(cacheKey)
+
+    if (cachedData) {
+      const { results, total_pages } = JSON.parse(cachedData)
+      setMovies(results)
+      setTotalPages(total_pages)
+      return
+    }
+
+    try {
+      const response = await fetch(
+        `/api/tmdb/top_rated?language=pt-BR&page=${page}`,
+        {
+          headers: {
+            Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+            accept: 'application/json',
+          },
+        },
+      )
+      if (!response.ok) {
+        throw new Error('Erro ao buscar filmes')
+      }
+      const data = await response.json()
+      setMovies(data.results)
+      setTotalPages(data.total_pages)
+
+      // Cache the data
+      localStorage.setItem(cacheKey, JSON.stringify(data))
+    } catch (error) {
+      setError(error.message)
+    }
+  }, [])
+
+  useEffect(() => {
+    fetchMovies(page)
+  }, [page, fetchMovies])
+
+  const handlePageChange = (
+    event: React.ChangeEvent<unknown>,
+    value: number,
+  ) => {
+    setPage(value)
+  }
+
+  return (
+    <HomeContainer>
+      {error && <Alert severity="error">{error}</Alert>}
+      <Pagination
+        count={totalPages}
+        page={page}
+        onChange={handlePageChange}
+        variant="outlined"
+      />
+      <MoviesGrid>
+        {movies.map((movie) => (
+          <MovieCard key={movie.id}>
+            <MovieTitle>{movie.title}</MovieTitle>
+            <MovieBanner
+              src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+              alt={movie.title}
+            />
+          </MovieCard>
+        ))}
+      </MoviesGrid>
+    </HomeContainer>
+  )
+}
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,57 @@
+import styled from 'styled-components'
+
+export const HomeContainer = styled.section`
+  max-width: 62.1vw;
+  @media (max-width: 999px) {
+    max-width: 89vw;
+    padding: 1rem;
+  }
+  padding: 1rem;
+  nav {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 2rem;
+  }
+`
+export const MoviesGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(10vw, 1fr));
+  gap: 1.5rem;
+
+  padding: 0.5rem;
+  @media (max-width: 999px) {
+    grid-template-columns: repeat(auto-fill, minmax(16vw, 1fr));
+    gap: 0.5rem;
+  }
+`
+
+export const MovieCard = styled.div`
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  overflow: hidden;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  transition: transform 0.3s ease;
+  width: 12rem;
+  @media (max-width: 999px) {
+    width: 10rem;
+  }
+  &:hover {
+    border: 1px solid red;
+    transform: scale(1.05);
+  }
+`
+
+export const MovieBanner = styled.img`
+  width: 100%;
+  height: 100%;
+`
+
+export const MovieTitle = styled.h2`
+  position: absolute;
+  margin-top: 4rem;
+  font-size: 12px;
+  padding: 12px;
+
+  text-align: center;
+`
