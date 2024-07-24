34ca436de79e243d46e4a84a4aee782e8aa9e8be
biel.lil <biel1313biel@gmail.com>
2024-07-24 14:52:20 -0300

    feat: :wrench: configuração do Vite: proxy para API do TMDB

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api/tmdb': {
+        target: 'https://api.themoviedb.org/3/movie',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/tmdb/, ''),
+      },
+    },
+  },
 })
