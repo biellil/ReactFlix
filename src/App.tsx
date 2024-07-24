7c18f850358ac4c8c30bdcbfcd594c4d36f8059f
biel.lil <biel1313biel@gmail.com>
2024-07-24 15:14:30 -0300

    ✨ Adiciona roteamento e novos componentes na aplicação
    Configura o roteamento usando BrowserRouter e adiciona novos componentes. Envolve o conteúdo principal com o componente MainHero.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import { BrowserRouter } from 'react-router-dom'
+import { Routers } from './Router'
+import { MainHero } from './styles/styles'
+
 export function App() {
-  return <></>
+  return (
+    <>
+      <MainHero>
+        <BrowserRouter>
+          <Routers />
+        </BrowserRouter>
+      </MainHero>
+    </>
+  )
 }
