08864c2f6f7d9c0f72be6b423f840963c384f393
biel.lil <biel1313biel@gmail.com>
2024-07-24 15:07:16 -0300

    :wrench: Adicionando novos temas ao componente

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
 import '@fontsource/roboto/300.css'
@@ -7,13 +6,14 @@ import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 import { ThemeProvider } from '@emotion/react'
 import { CssBaseline } from '@mui/material'
-import { GlobalStyle, theme } from './styles/global.ts'
+import { GlobalStyle, darkTheme } from './styles/global.ts'
+import React from 'react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <GlobalStyle />
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={darkTheme}>
       <CssBaseline />
+      <GlobalStyle />
       <App />
     </ThemeProvider>
   </React.StrictMode>,
