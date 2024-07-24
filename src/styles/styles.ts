b9447a9b8fda229ae844620188255c138595d3e5
biel.lil <biel1313biel@gmail.com>
2024-07-24 15:12:32 -0300

    ✨ Criando componente LayoutDefault para definir o layout padrão
    Adicionado o componente LayoutDefault que estabelece o layout padrão da aplicação. Esse componente define a estrutura básica para as páginas e garante consistência no layout.

---
+++ src/styles/styles.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+export const MainHero = styled.section`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+  width: 100vw;
+  .container {
+    width: 90vw;
+    max-width: 75rem;
+    background-color: rgb(15 15 15 / 0.5);
+    margin-top: 5rem;
+    margin-right: auto;
+    margin-left: auto;
+    padding: 1rem 0.5rem;
+    margin-bottom: 1rem;
+    border-radius: 25px;
+    display: flex;
+    flex-direction: column;
+  }
+  @media (max-width: 768px) {
+    .container {
+      width: 90%;
+      max-width: 70rem;
+    }
+  }
+`
