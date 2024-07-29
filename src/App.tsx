985feabc044ab3e5eee2864f8a169e82a3242a82
biel.lil <biel1313biel@gmail.com>
2024-07-29 09:57:14 -0300

    🚀 Implementa Firebase Analytics com variáveis de ambiente

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import { BrowserRouter } from 'react-router-dom'
 import { Routers } from './Router'
 import { MainHero } from './styles/styles'
+import React, { useEffect } from 'react'
+import { analytics } from './components/firebase'
+import { logEvent } from 'firebase/analytics'
 
 export function App() {
+  useEffect(() => {
+    logEvent(analytics, 'page_view')
+  }, [])
+
   return (
-    <>
-      <MainHero>
-        <BrowserRouter>
-          <Routers />
-        </BrowserRouter>
-      </MainHero>
-    </>
+    <MainHero>
+      <BrowserRouter>
+        <Routers />
+      </BrowserRouter>
+    </MainHero>
   )
 }
