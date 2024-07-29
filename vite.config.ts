1616323d02b93294da8b2bdd6cb9a365b875e113
biel.lil <biel1313biel@gmail.com>
2024-07-29 16:18:49 -0300

    feat: otimizar a configuração de build do Vite para melhor desempenho
    - Adicionado rollup-plugin-visualizer para analisar o tamanho e a composição do bundle
    - Implementado vite-plugin-compression com compressão Brotli para reduzir o tamanho dos assets
    - Configurado manualChunks em rollupOptions para melhor divisão de código
    - Incluído plugin react para suporte ao React
    - Atualizadas as configurações de build para desempenho aprimorado

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,30 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { visualizer } from 'rollup-plugin-visualizer'
+import viteCompression from 'vite-plugin-compression'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api/tmdb': {
-        target: 'https://api.themoviedb.org/3/movie',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/tmdb/, ''),
+  plugins: [
+    react(),
+    visualizer({
+      filename: './dist/stats.html',
+      open: true,
+    }),
+    viteCompression({ algorithm: 'brotliCompress' }),
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString()
+          }
+        },
       },
     },
   },
