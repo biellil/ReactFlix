5c7f5626cc1837badd6bf51f7b4291fb4c9b2a3c
biel.lil <biel1313biel@gmail.com>
2024-08-04 14:25:49 -0300

    Update styles.ts

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -2,16 +2,17 @@ import styled from 'styled-components'
 
 export const MainHero = styled.section`
   display: flex;
-  justify-content: center;
+  justify-content: start;
   align-items: center;
   flex-direction: column;
   height: 100vh;
   width: 100vw;
   .container {
+    height: 90vh;
     width: 90vw;
     max-width: 75rem;
     background-color: rgb(15 15 15 / 0.5);
-    margin-top: 5rem;
+    margin-top: 1.5rem;
     margin-right: auto;
     margin-left: auto;
     padding: 1rem 0.5rem;
