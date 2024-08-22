95ae24da1009839bd7eb69c59583375bf3757337
biel.lil <biel1313biel@gmail.com>
2024-08-22 12:05:11 -0300

    fix tv

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -2,34 +2,38 @@ import styled from 'styled-components'
 
 export const MainHero = styled.section`
   display: flex;
-  justify-content: start;
+  justify-content: center;
   align-items: center;
-  flex-direction: column;
-  height: 100vh;
-  width: 100vw;
+
+  height: 99vh;
+  width: 99vw;
   .container {
-    height: 85vh;
-    width: 95vw;
-    max-width: 75rem;
+    height: 99vh;
+    width: 99vw;
     background-color: rgb(15 15 15 / 0.7);
-    margin-top: 1.5rem;
-    margin-right: auto;
-    margin-left: auto;
-    padding: 1rem 0.5rem;
-    margin-bottom: 1rem;
     border-radius: 25px;
+    gap: 2rem;
     display: flex;
-    flex-direction: column;
   }
-  @media (max-width: 999px) {
+  @media screen and (min-width: 1920px) {
     .container {
       height: 95vh;
     }
   }
-  @media (max-width: 768px) {
+  @media (max-width: 1024px) {
+    justify-content: start;
+    flex-direction: column;
     .container {
-      width: 81vw;
+      width: 95vw;
+      max-width: 75rem;
+      width: 82vw;
       max-width: 70rem;
+      margin-top: 1.5rem;
+      margin-right: auto;
+      margin-left: auto;
+      padding: 1rem 0.5rem;
+      margin-bottom: 1rem;
+      flex-direction: column;
     }
   }
 `
