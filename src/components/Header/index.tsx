61c3dd3a55fe1006aae47d9059f1a2a6412fab04
biel.lil <biel1313biel@gmail.com>
2024-11-10 11:51:35 -0300

    feat: adiciona funcionalidade de sign-out ao componente Header
    - Implementa botão SignOut no componente Header para realizar logout do usuário
    - Adiciona lógica para remover dados de autenticação do local storage ao fazer sign-out
    - Atualiza interface para incluir o ícone SignOut com evento de clique (onClick)
    - Inclui reload opcional da página após logout para redirecionamento à página de login
    
    Essa funcionalidade melhora a segurança e a experiência do usuário ao garantir que dados de autenticação sejam removidos ao sair.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,14 @@
-import React, { useState } from 'react'
-import { FilmReel, FilmScript } from '@phosphor-icons/react'
-import { Category, Headers } from './styles'
-import icon from '../../assets/icon.png'
-import { SearchInput } from '../SearchInput'
+import React, { useState } from "react";
+import { FilmReel, FilmScript, SignOut } from "@phosphor-icons/react";
+import { Category, DivSignOut, Headers } from "./styles";
+import icon from "../../assets/icon.png";
+import { SearchInput } from "../SearchInput";
+import { getAuth, signOut } from "firebase/auth"; // Importe o signOut do Firebase
 
 interface HeaderProps {
-  onCategoryChange: (category: string) => void
-  onSearchChange: (searchTerm: string) => void
-  searchTerm: string
+  onCategoryChange: (category: string) => void;
+  onSearchChange: (searchTerm: string) => void;
+  searchTerm: string;
 }
 
 export function Header({
@@ -15,33 +16,48 @@ export function Header({
   onSearchChange,
   searchTerm,
 }: HeaderProps) {
-  const [activeCategory, setActiveCategory] = useState<string>('Filmes')
+  const [activeCategory, setActiveCategory] = useState<string>("Filmes");
 
   const handleCategoryChange = (category: string) => {
-    setActiveCategory(category)
-    onCategoryChange(category)
-  }
+    setActiveCategory(category);
+    onCategoryChange(category);
+  };
+
+  const handleSignOut = () => {
+    const auth = getAuth(); // Obtenha a instância de autenticação do Firebase
+    signOut(auth)
+      .then(() => {
+        localStorage.removeItem("user"); // Remove o dado do usuário do localStorage
+        window.location.href = "/Auth"; // Redireciona para a página de autenticação
+      })
+      .catch((error) => {
+        console.error("Erro ao fazer sign out:", error);
+      });
+  };
 
   return (
     <Headers>
       <img src={icon} alt="Ícone" />
       <Category>
         <div
-          onClick={() => handleCategoryChange('Filmes')}
-          className={activeCategory === 'Filmes' ? 'active' : ''}
+          onClick={() => handleCategoryChange("Filmes")}
+          className={activeCategory === "Filmes" ? "active" : ""}
         >
           <FilmReel size={32} weight="fill" />
           <h2>Filmes</h2>
         </div>
         <div
-          onClick={() => handleCategoryChange('Series')}
-          className={activeCategory === 'Series' ? 'active' : ''}
+          onClick={() => handleCategoryChange("Series")}
+          className={activeCategory === "Series" ? "active" : ""}
         >
           <FilmScript size={32} weight="fill" />
           <h2>Series</h2>
         </div>
       </Category>
+      <DivSignOut>
+        <SignOut size={30} onClick={handleSignOut} />
+      </DivSignOut>
       <SearchInput searchTerm={searchTerm} onSearchChange={onSearchChange} />
     </Headers>
-  )
+  );
 }
